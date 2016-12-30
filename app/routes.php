821c58f3958ed129d845eee2bd863944fbb1a48a
alenkalac <alenkalac87@gmail.com>
2016-12-30 02:56:05 +0000

    Fixed the 404 for route /login

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,7 @@
 
 	$app->get('/', "Alen\MainController::indexPage");
 	$app->get('/signup', "Alen\MainController::signupPage");
+	$app->get('/login', "Alen\MainController::signupPage");
 	$app->get('/validate/{email}/{token}', "Alen\MainController::validateProcess");
 
 	$app->post("/login", "Alen\MainController::loginProcess");
